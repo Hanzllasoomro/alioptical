8a6bd011094c49f0d940dc0b91081baccba69880
PMYLS <shaikhkabeer069@gmail.com>
2025-10-26 01:48:38 +0500

    Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
+import 'package:alioptical/servers/auth/auth_gate.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import './pages/dashboard.dart';
 
-void main() async {
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
 
+  // 👇 Check Firebase connection
+  print("🔥 Firebase connected: ${Firebase.apps.isNotEmpty}");
+
+  runApp(const MyApp());
 }
+
 
 
 class MyApp extends StatelessWidget {
@@ -22,12 +28,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ali Optical',
+      title: 'Ali Ocal',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       ),
-      home: const DashboardScreen(),
+      home: const AuthGate(),
     );
   }
 }
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:alioptical/servers/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import './customerSearchScreen.dart';
@@ -5,6 +6,11 @@ import './customerSearchScreen.dart';
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
+  void logout(){
+    final _authService= AuthService();
+    _authService.signOut();
+
+  }
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -16,42 +22,42 @@ class DashboardScreen extends StatelessWidget {
         child: Column(
           children: [
             // 🔹 Header section
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(
-                vertical: isWide ? 50 : 40,
-                horizontal: isWide ? 40 : 20,
-              ),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFFB71C1C), Color(0xFFD32F2F)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Icon(
-                    Icons.store,
-                    color: Colors.white,
-                    size: isWide ? 80 : 60,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    "Tariq Eye Corner",
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: isWide ? 28 : 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // Container(
+            //   width: double.infinity,
+            //   padding: EdgeInsets.symmetric(
+            //     vertical: isWide ? 50 : 40,
+            //     horizontal: isWide ? 40 : 20,
+            //   ),
+            //   decoration: const BoxDecoration(
+            //     gradient: LinearGradient(
+            //       colors: [Color(0xFFB71C1C), Color(0xFFD32F2F)],
+            //       begin: Alignment.topCenter,
+            //       end: Alignment.bottomCenter,
+            //     ),
+            //     borderRadius: BorderRadius.only(
+            //       bottomLeft: Radius.circular(50),
+            //       bottomRight: Radius.circular(50),
+            //     ),
+            //   ),
+            //   child: Column(
+            //     children: [
+            //       Icon(
+            //         Icons.store,
+            //         color: Colors.white,
+            //         size: isWide ? 80 : 60,
+            //       ),
+            //       const SizedBox(height: 10),
+            //       Text(
+            //         "Tariq Eye Corner",
+            //         style: GoogleFonts.poppins(
+            //           color: Colors.white,
+            //           fontSize: isWide ? 28 : 22,
+            //           fontWeight: FontWeight.bold,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
 
             const SizedBox(height: 30),
 
@@ -126,7 +132,7 @@ class DashboardScreen extends StatelessWidget {
                         _DashboardButton(
                           icon: Icons.power_settings_new,
                           label: "Logout",
-                          onTap: () {},
+                          onTap: logout,
                           isWide: isWide,
                         ),
                       ],
